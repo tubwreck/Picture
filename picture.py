850b31daf7262fc5da0cbd19b535c800e694c9e0
Eric Dennison <ericd@netdenizen.com>
2015-09-07 17:15:18 -0400

    Update picture.py

--- picture.py
+++ picture.py
@@ -5,5 +5,32 @@ Credit: <list sources used, if any>
 
 Assignment:
 
-something here
+Use the ggame library to "paint" a graphical picture of something (e.g. a house, a face or landscape).
+
+Use at least:
+1. Three different Color objects.
+2. Ten different Sprite objects.
+3. One (or more) RectangleAsset objects.
+4. One (or more) CircleAsset objects.
+5. One (or more) EllipseAsset objects.
+6. One (or more) PolygonAsset objects.
+
+See:
+https://github.com/HHS-IntroProgramming/Standards-and-Syllabus/wiki/TUTORIAL:-Displaying-Graphics
+for general information on how to use ggame.
+
+See:
+http://brythonserver.github.io/ggame/
+for detailed information on ggame.
+
 """
+from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset
+
+# add your code here \/  \/  \/
+
+
+# add your code here /\  /\  /\
+
+
+myapp = App()
+myapp.run()
